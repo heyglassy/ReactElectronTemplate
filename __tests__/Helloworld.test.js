182e6c69ab1c24cf38db806e162cc2172cc228cf
Christian Glassiognon <chrisgglassi@gmail.com>
2021-07-24 00:41:54 -0500

    Removed mocha

--- __tests__/Helloworld.test.js
+++ __tests__/Helloworld.test.js
@@ -1,9 +1,19 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React, { useState as useStateMock } from "react";
+import { fireEvent, render } from "@testing-library/react";
 import HelloWorld from "../src/components/HelloWorld";
-import { expect } from "@jest/globals";
+import userEvent from "@testing-library/user-event";
+
+jest.mock("react", () => ({
+  ...jest.requireActual("react"),
+  useState: jest.fn(),
+}));
+
+const setState = jest.fn();
 
 describe("test", () => {
+  beforeAll(() => {
+    useStateMock.mockImplementation((init) => [init, setState]);
+  });
   it("tests", () => {
     const element = document.createElement("div");
     const { container } = render(<HelloWorld />, {
@@ -18,5 +28,16 @@ describe("test", () => {
       container: document.body.appendChild(element),
     });
     expect(container.firstChild.textContent).toBe("Hello World!");
+  });
+
+  it("test 3", () => {
+    const element = document.createElement("div");
+    const { container } = render(<HelloWorld />, {
+      container: document.body.appendChild(element),
+    });
+    container.getElementsByClassName("text");
+
+    userEvent.type(container.querySelector(".text"), "test");
+    expect(setState).toBeCalled();
   });
 });
